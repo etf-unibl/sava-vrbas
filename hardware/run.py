4aab220d728d6b47a131ac35269882ffa8a5f62d
Emanuela Buganik <emanuela.buganik@student.etf.unibl.org>
2023-02-01 13:54:59 +0100

    Testbench path in run.py updated

--- hardware/run.py
+++ hardware/run.py
@@ -5,7 +5,7 @@ from vunit import VUnit
 ROOT = Path(__file__).resolve().parent
 vu = VUnit.from_argv()
 SRC_PATH = ROOT / "design"/"*"
-TB_PATH = ROOT / "design"/"*"/"testbench"
+TB_PATH = ROOT / "tests"
 vu.add_library("common_lib").add_source_files(SRC_PATH / "*.vhd")
 vu.add_library("tb_common_lib").add_source_files(TB_PATH / "*.vhd")
 
