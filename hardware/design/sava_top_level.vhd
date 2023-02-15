2262b86cdafac27d0fe89b03ae31e4dc89e027bb
Nemanja0 <ncenic39@gmail.com>
2023-02-15 17:52:43 +0100

    Issue #34 : Top level design for project - Added top level design

--- hardware/design/sava_top_level.vhd
+++ hardware/design/sava_top_level.vhd
@@ -4,11 +4,11 @@
 -- https://github.com/knezicm/sava-vrbas/
 -----------------------------------------------------------------------------
 --
--- unit name:     SAVA TOP LEVEL DESIGN UNIT
+-- unit name: SAVA TOP LEVEL DESIGN UNIT
 --
 -- description:
 --
--- This file is used for instantiation of all design units.
+--   This file implements top module for TX and RX
 --
 -----------------------------------------------------------------------------
 -- Copyright (c) 2022 Faculty of Electrical Engineering
@@ -35,3 +35,76 @@
 -- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 -- OTHER DEALINGS IN THE SOFTWARE
 -----------------------------------------------------------------------------
+--! @file
+--! @brief
+-----------------------------------------------------------------------------
+--! Use standard library
+library ieee;
+--! Use logic elements
+use ieee.std_logic_1164.all;
+--! Use numeric elements
+use ieee.numeric_std.all;
+
+--! @brief 
+--! @details
+entity sava_top_level is
+  port (
+    clk_i : in  std_logic; --! Input clock signal
+    scl_i : in  std_logic; --! Input i2s clock signal
+    ws_i  : in  std_logic; --! Input word select signal
+    sd_i  : in  std_logic; --! Input serial data signal
+    sd_o  : out std_logic --! Output serial data signal
+  );
+end sava_top_level;
+
+--! @brief
+--! @details
+architecture arch of sava_top_level is
+  component rx
+    port (
+      clk_i    : in  std_logic; --! Input clock signal
+      scl_i    : in  std_logic; --! Input i2s clock signal
+      ws_i     : in  std_logic; --! Input word select signal
+      sd_i     : in  std_logic; --! Inpu serial data signal
+      data_l_o : out std_logic_vector(23 downto 0); --! Output signal for left channel
+      data_r_o : out std_logic_vector(23 downto 0) --! Output signal for right channel
+    );
+  end component;
+  component tx
+    port (
+      clk_i    : in  std_logic; --! Input clock signal
+      scl_i    : in  std_logic; --! Input i2s clock signal
+      ws_i     : in  std_logic; --! Input word select signal
+      data_l_i : in std_logic_vector(23 downto 0); --! Input buffer for left channel
+      data_r_i : in std_logic_vector(23 downto 0); --! Input buffer for right channel
+      sd_o     : out  std_logic --! Output serial data signal
+    );
+  end component;
+
+  signal data_l, data_r : std_logic_vector(23 downto 0) := (others => '0');
+begin
+  receiver : rx
+  port map(clk_i => clk_i,
+           scl_i => scl_i,
+			  ws_i  => ws_i,
+			  sd_i  => sd_i,
+			  data_l_o => data_l,
+			  data_r_o => data_r);
+
+  transmitter : tx
+  port map(clk_i => clk_i,
+           scl_i => scl_i,
+			  ws_i  => ws_i,
+			  data_l_i => data_l,
+			  data_r_i => data_r,
+			  sd_o => sd_o);
+end arch;
+
+  transmitter : tx
+  port map(clk_i => clk_i,
+           scl_i => scl_i,
+			  ws_i  => ws_i,
+			  data_l_i => data_l,
+			  data_r_i => data_r,
+			  sd_o => sd_o);
+end arch;
