fc1ba7a1a141cda86bf5a03478f8ba3806f49c60
Nikola Cetic <nikolacetic8@gmail.com>
2023-02-16 18:42:22 +0100

    Issue #32 : Implementation of i2c interface Created falling and rising edge detectors Created ALTIOBUFers Implemented I2C interface

--- software/hw/i2c_interface/altiobuf_i.vhd
+++ software/hw/i2c_interface/altiobuf_i.vhd
@@ -1,12 +1,12 @@
 -- megafunction wizard: %ALTIOBUF%
 -- GENERATION: STANDARD
 -- VERSION: WM1.0
--- MODULE: altiobuf_out 
+-- MODULE: altiobuf_in 
 
 -- ============================================================
--- File Name: altiobuf_o.vhd
+-- File Name: altiobuf_i.vhd
 -- Megafunction Name(s):
--- 			altiobuf_out
+-- 			altiobuf_in
 --
 -- Simulation Library Files(s):
 -- 			cyclonev
@@ -34,90 +34,81 @@
 --https://fpgasoftware.intel.com/eula.
 
 
---altiobuf_out CBX_AUTO_BLACKBOX="ALL" DEVICE_FAMILY="Cyclone V" ENABLE_BUS_HOLD="FALSE" LEFT_SHIFT_SERIES_TERMINATION_CONTROL="FALSE" NUMBER_OF_CHANNELS=1 OPEN_DRAIN_OUTPUT="TRUE" PSEUDO_DIFFERENTIAL_MODE="FALSE" USE_DIFFERENTIAL_MODE="FALSE" USE_OE="TRUE" USE_TERMINATION_CONTROL="FALSE" datain dataout oe
---VERSION_BEGIN 20.1 cbx_altiobuf_out 2020:11:11:17:06:45:SJ cbx_mgl 2020:11:11:17:08:38:SJ cbx_stratixiii 2020:11:11:17:06:46:SJ cbx_stratixv 2020:11:11:17:06:46:SJ  VERSION_END
+--altiobuf_in CBX_AUTO_BLACKBOX="ALL" DEVICE_FAMILY="Cyclone V" ENABLE_BUS_HOLD="FALSE" NUMBER_OF_CHANNELS=1 USE_DIFFERENTIAL_MODE="FALSE" USE_DYNAMIC_TERMINATION_CONTROL="FALSE" datain dataout
+--VERSION_BEGIN 20.1 cbx_altiobuf_in 2020:11:11:17:06:45:SJ cbx_mgl 2020:11:11:17:08:38:SJ cbx_stratixiii 2020:11:11:17:06:46:SJ cbx_stratixv 2020:11:11:17:06:46:SJ  VERSION_END
 
  LIBRARY cyclonev;
  USE cyclonev.all;
 
---synthesis_resources = cyclonev_io_obuf 1 
+--synthesis_resources = cyclonev_io_ibuf 1 
  LIBRARY ieee;
  USE ieee.std_logic_1164.all;
 
- ENTITY  altiobuf_o_iobuf_out_krs IS 
+ ENTITY  altiobuf_i_iobuf_in_v0i IS 
 	 PORT 
 	 ( 
 		 datain	:	IN  STD_LOGIC_VECTOR (0 DOWNTO 0);
-		 dataout	:	OUT  STD_LOGIC_VECTOR (0 DOWNTO 0);
-		 oe	:	IN  STD_LOGIC_VECTOR (0 DOWNTO 0) := (OTHERS => '1')
+		 dataout	:	OUT  STD_LOGIC_VECTOR (0 DOWNTO 0)
 	 ); 
- END altiobuf_o_iobuf_out_krs;
+ END altiobuf_i_iobuf_in_v0i;
 
- ARCHITECTURE RTL OF altiobuf_o_iobuf_out_krs IS
+ ARCHITECTURE RTL OF altiobuf_i_iobuf_in_v0i IS
 
-	 SIGNAL  wire_obufa_o	:	STD_LOGIC;
-	 SIGNAL  oe_w :	STD_LOGIC_VECTOR (0 DOWNTO 0);
-	 COMPONENT  cyclonev_io_obuf
+	 SIGNAL  wire_ibufa_o	:	STD_LOGIC;
+	 COMPONENT  cyclonev_io_ibuf
 	 GENERIC 
 	 (
 		bus_hold	:	STRING := "false";
-		open_drain_output	:	STRING := "false";
-		shift_series_termination_control	:	STRING := "false";
-		lpm_type	:	STRING := "cyclonev_io_obuf"
+		differential_mode	:	STRING := "false";
+		simulate_z_as	:	STRING := "z";
+		lpm_type	:	STRING := "cyclonev_io_ibuf"
 	 );
 	 PORT
 	 ( 
 		dynamicterminationcontrol	:	IN STD_LOGIC := '0';
 		i	:	IN STD_LOGIC := '0';
-		o	:	OUT STD_LOGIC;
-		obar	:	OUT STD_LOGIC;
-		oe	:	IN STD_LOGIC := '1';
-		parallelterminationcontrol	:	IN STD_LOGIC_VECTOR(15 DOWNTO 0) := (OTHERS => '0');
-		seriesterminationcontrol	:	IN STD_LOGIC_VECTOR(15 DOWNTO 0) := (OTHERS => '0')
+		ibar	:	IN STD_LOGIC := '0';
+		o	:	OUT STD_LOGIC
 	 ); 
 	 END COMPONENT;
  BEGIN
 
-	dataout(0) <= wire_obufa_o;
-	oe_w <= oe;
-	obufa :  cyclonev_io_obuf
+	dataout(0) <= wire_ibufa_o;
+	ibufa :  cyclonev_io_ibuf
 	  GENERIC MAP (
 		bus_hold => "false",
-		open_drain_output => "true"
+		differential_mode => "false"
 	  )
 	  PORT MAP ( 
 		i => datain(0),
-		o => wire_obufa_o,
-		oe => oe_w(0)
+		o => wire_ibufa_o
 	  );
 
- END RTL; --altiobuf_o_iobuf_out_krs
+ END RTL; --altiobuf_i_iobuf_in_v0i
 --VALID FILE
 
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
-ENTITY altiobuf_o IS
+ENTITY altiobuf_i IS
 	PORT
 	(
 		datain		: IN STD_LOGIC_VECTOR (0 DOWNTO 0);
-		oe		: IN STD_LOGIC_VECTOR (0 DOWNTO 0);
 		dataout		: OUT STD_LOGIC_VECTOR (0 DOWNTO 0)
 	);
-END altiobuf_o;
+END altiobuf_i;
 
 
-ARCHITECTURE RTL OF altiobuf_o IS
+ARCHITECTURE RTL OF altiobuf_i IS
 
 	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (0 DOWNTO 0);
 
 
 
-	COMPONENT altiobuf_o_iobuf_out_krs
+	COMPONENT altiobuf_i_iobuf_in_v0i
 	PORT (
 			datain	: IN STD_LOGIC_VECTOR (0 DOWNTO 0);
-			oe	: IN STD_LOGIC_VECTOR (0 DOWNTO 0);
 			dataout	: OUT STD_LOGIC_VECTOR (0 DOWNTO 0)
 	);
 	END COMPONENT;
@@ -125,10 +116,9 @@ ARCHITECTURE RTL OF altiobuf_o IS
 BEGIN
 	dataout    <= sub_wire0(0 DOWNTO 0);
 
-	altiobuf_o_iobuf_out_krs_component : altiobuf_o_iobuf_out_krs
+	altiobuf_i_iobuf_in_v0i_component : altiobuf_i_iobuf_in_v0i
 	PORT MAP (
 		datain => datain,
-		oe => oe,
 		dataout => sub_wire0
 	);
 
@@ -144,22 +134,16 @@ END RTL;
 -- Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
 -- Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone V"
 -- Retrieval info: CONSTANT: enable_bus_hold STRING "FALSE"
--- Retrieval info: CONSTANT: left_shift_series_termination_control STRING "FALSE"
 -- Retrieval info: CONSTANT: number_of_channels NUMERIC "1"
--- Retrieval info: CONSTANT: open_drain_output STRING "TRUE"
--- Retrieval info: CONSTANT: pseudo_differential_mode STRING "FALSE"
 -- Retrieval info: CONSTANT: use_differential_mode STRING "FALSE"
--- Retrieval info: CONSTANT: use_oe STRING "TRUE"
--- Retrieval info: CONSTANT: use_termination_control STRING "FALSE"
+-- Retrieval info: CONSTANT: use_dynamic_termination_control STRING "FALSE"
 -- Retrieval info: USED_PORT: datain 0 0 1 0 INPUT NODEFVAL "datain[0..0]"
 -- Retrieval info: USED_PORT: dataout 0 0 1 0 OUTPUT NODEFVAL "dataout[0..0]"
--- Retrieval info: USED_PORT: oe 0 0 1 0 INPUT NODEFVAL "oe[0..0]"
 -- Retrieval info: CONNECT: @datain 0 0 1 0 datain 0 0 1 0
--- Retrieval info: CONNECT: @oe 0 0 1 0 oe 0 0 1 0
 -- Retrieval info: CONNECT: dataout 0 0 1 0 @dataout 0 0 1 0
--- Retrieval info: GEN_FILE: TYPE_NORMAL altiobuf_o.vhd TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL altiobuf_o.inc FALSE
--- Retrieval info: GEN_FILE: TYPE_NORMAL altiobuf_o.cmp TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL altiobuf_o.bsf FALSE
--- Retrieval info: GEN_FILE: TYPE_NORMAL altiobuf_o_inst.vhd TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL altiobuf_i.vhd TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL altiobuf_i.inc FALSE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL altiobuf_i.cmp TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL altiobuf_i.bsf FALSE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL altiobuf_i_inst.vhd TRUE
 -- Retrieval info: LIB_FILE: cyclonev
--- software/hw/i2c_interface/i2c_interface.vhd
+++ software/hw/i2c_interface/i2c_interface.vhd
@@ -6,11 +6,12 @@ entity i2c_interface is
 	port
 	(
 		-- Input ports
-		clk_i			: in  std_logic;
-		rpi_sda_io	: inout  std_logic_vector(0 downto 0);
-		scl_i			: in  std_logic;
-		scl_o			: out	std_logic;
-		codec_sda_io	: inout	std_logic_vector(0 downto 0)
+		clk_i				: in  	std_logic;
+		rpi_sda_io		: inout  std_logic_vector(0 downto 0);
+		scl_i				: in  	std_logic;
+		scl_o				: out		std_logic;
+		codec_sda_io	: inout	std_logic_vector(0 downto 0);
+		codec_xck		: out		std_logic
 	);
 end i2c_interface;
 
@@ -18,7 +19,7 @@ end i2c_interface;
 
 architecture arch of i2c_interface is
 
-type t_state is (idle, codec_low, rpi_low);
+type t_state is (codec, rpi,idle);
 signal state_reg  : t_state;
 signal state_next : t_state;
 
@@ -28,20 +29,52 @@ signal codec_oe				:   STD_LOGIC_VECTOR (0 DOWNTO 0);
 signal rpi_sda_o				:   STD_LOGIC_VECTOR (0 DOWNTO 0);
 signal rpi_oe					:   STD_LOGIC_VECTOR (0 DOWNTO 0);
 
-signal codec_edge				:	 std_logic;
-signal rpi_edge				:	 std_logic;
+signal codec_edge_falling				:	 std_logic;
+signal rpi_edge_falling				:	 std_logic;
+signal codec_edge_rising				:	 std_logic;
+signal rpi_edge_rising				:	 std_logic;
+
+
+
+signal		codec_clk :  std_logic;        -- outclk0.clk
+signal		locked   :  std_logic;         --  locked.export
+component pll_12MHz is
+	port (
+		refclk   : in  std_logic := '0'; --  refclk.clk
+		rst      : in  std_logic := '0'; --   reset.reset
+		outclk_0 : out std_logic;        -- outclk0.clk
+		locked   : out std_logic         --  locked.export
+	);
+end component;
+
 
 component altiobuf_o IS
 	PORT
 	(
 		datain		: IN STD_LOGIC_VECTOR (0 DOWNTO 0);
 		oe		: IN STD_LOGIC_VECTOR (0 DOWNTO 0);
-		dataio		: INOUT STD_LOGIC_VECTOR (0 DOWNTO 0);
 		dataout		: OUT STD_LOGIC_VECTOR (0 DOWNTO 0)
 	);
 END component;
 
-  component dual_edge_detector
+component altiobuf_i IS
+	PORT
+	(
+		datain		: IN STD_LOGIC_VECTOR (0 DOWNTO 0);
+		dataout		: OUT STD_LOGIC_VECTOR (0 DOWNTO 0)
+	);
+END component;
+
+  component rising_edge_detector
+    port(
+      clk_i      : in   std_logic;
+      rst_i      : in   std_logic;
+      strobe_i   : in   std_logic;
+      p_o        : out  std_logic
+    );
+  end component;
+  
+  component falling_edge_detector
     port(
       clk_i      : in   std_logic;
       rst_i      : in   std_logic;
@@ -52,34 +85,59 @@ END component;
 
 begin
 
-codec_altiobuf : altiobuf_o 
-PORT MAP (
-		datain	 => rpi_sda_o,
-		oe	       => codec_oe,
-		dataio	 => codec_sda_io,
-		dataout	 => codec_sda_o
+codec_xck_inst : pll_12MHz
+port map(
+	refclk 	=> clk_i,
+	rst 		=> '0',
+	outclk_0 => codec_clk,
+	locked	=> locked
 	);
 
-	
-rpi_altiobuf : altiobuf_o 
-PORT MAP (
-		datain	 => codec_sda_o,
-		oe	 		 => rpi_oe,
-		dataio	 => rpi_sda_io,
-		dataout	 => rpi_sda_o
+output_codec : altiobuf_o PORT MAP (
+		datain	 => rpi_sda_o,
+		oe	 => codec_oe,
+		dataout	 => codec_sda_io
 	);
 	
-codec_edge_detector : dual_edge_detector
+output_rpi : altiobuf_o PORT MAP (
+		datain	 => codec_sda_o,
+		oe	 => rpi_oe,
+		dataout	 => rpi_sda_io
+	);
+	
+input_rpi : altiobuf_i PORT MAP (
+		datain	 => rpi_sda_io,
+		dataout	 => rpi_sda_o
+	);
+
+input_codec : altiobuf_i PORT MAP (
+		datain	 => codec_sda_io,
+		dataout	 => codec_sda_o
+	);
+	
+codec_edge_detector_rising : rising_edge_detector
   port map(clk_i    => clk_i,
            rst_i    => '0',
            strobe_i => codec_sda_o(0),
-           p_o      => codec_edge);
+           p_o      => codec_edge_rising);
 			  
-rpi_edge_detector : dual_edge_detector
+rpi_edge_detector_rising : rising_edge_detector
   port map(clk_i    => clk_i,
            rst_i    => '0',
            strobe_i => rpi_sda_o(0),
-           p_o      => rpi_edge);
+           p_o      => rpi_edge_rising);
+			  
+codec_edge_detector_falling : falling_edge_detector
+  port map(clk_i    => clk_i,
+           rst_i    => '0',
+           strobe_i => codec_sda_o(0),
+           p_o      => codec_edge_falling);
+			  
+rpi_edge_detector_falling : falling_edge_detector
+  port map(clk_i    => clk_i,
+           rst_i    => '0',
+           strobe_i => rpi_sda_o(0),
+           p_o      => rpi_edge_falling);
 
 -- state register
   process(clk_i) is
@@ -91,29 +149,23 @@ rpi_edge_detector : dual_edge_detector
   
   
   -- next state logic
-  process(state_reg,rpi_edge,codec_edge) is
+  process(state_reg,rpi_edge_rising,rpi_edge_falling) is
   begin
    case state_reg is
+		when codec =>
+			if rpi_edge_falling = '1' then
+				state_next <= rpi;
+			else
+				state_next <= codec;
+			end if;
+		when rpi =>
+			if rpi_edge_rising = '1' then
+				state_next <= idle;
+			else
+				state_next <= rpi;
+			end if;
 		when idle =>
-			if rpi_edge = '1' then
-				state_next <= rpi_low;
-			elsif codec_edge = '1' then
-				state_next <= codec_low;
-			else
-				state_next <= idle;
-			end if;
-		when rpi_low =>
-			if rpi_edge = '1' then
-				state_next <= idle;
-			else
-				state_next <= rpi_low;
-			end if;
-		when codec_low =>
-			if codec_edge = '1' then
-				state_next <= idle;
-			else
-				state_next <= codec_low;
-			end if;
+			state_next <= codec;
 	end case;
   end process;
   
@@ -122,18 +174,23 @@ rpi_edge_detector : dual_edge_detector
   process(state_reg) is
   begin
 	case state_reg is
-		when idle =>
-			codec_oe <= (others => '1');
-			rpi_oe	<= (others => '1');
-		when rpi_low =>
-			codec_oe <= (others => '0');
-			rpi_oe 	<= (others => '1');
-		when codec_low =>
+		when rpi =>
 			codec_oe <= (others => '1');
 			rpi_oe 	<= (others => '0');
+		when codec =>
+			codec_oe <= (others => '0');
+			rpi_oe 	<= (others => '1');
+		when idle =>
+			codec_oe <= (others => '0');
+			rpi_oe 	<= (others => '1');
+			
 	end case;
   end process;
+
+--codec_oe <= (others => '1');
+--rpi_oe	<= (others => '0');
   
   -- concurrent
   scl_o 				<= scl_i;
+  codec_xck			<= codec_clk;
 end arch;
